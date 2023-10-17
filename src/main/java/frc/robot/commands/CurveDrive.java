49248d4b8cefe32f8d3550a6f9ba326fc3bd3473
Cardinal156 <2027156@scprep.org>
2023-10-17 17:58:25 -0400

    init(): Initial commit

---
+++ src/main/java/frc/robot/commands/CurveDrive.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.DriveTrain;
+
+public class CurveDrive extends CommandBase {
+  public CurveDrive(DriveTrain train) {
+    addRequirements(RobotContainer.m_driveTrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    final double speed = (Math.pow(RobotContainer.controller.getRightTriggerAxis(), 3) - Math.pow(RobotContainer.controller.getLeftTriggerAxis(), 3));
+    final double turn = RobotContainer.controller.getLeftX();
+
+    RobotContainer.m_driveTrain.getDifferentialDrive()
+      .curvatureDrive(speed, -turn * .6, false);
+
+    if(speed != 0) return;
+    double rotation = RobotContainer.controller.getLeftX();
+    if(Math.abs(rotation) < .1) return; 
+
+    RobotContainer.m_driveTrain.rotate(rotation);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class DriveTrain extends SubsystemBase {
+  private TalonSRX rightMotor = new TalonSRX(1);
+  private TalonSRX leftMotor = new TalonSRX(0);
+  private DifferentialDrive diffControl;
+
+  /** Creates a new DriveTrain. */
+  public DriveTrain() {
+    rightMotor.setInverted(false);
+    leftMotor.setInverted(false);
+
+    diffControl = new DifferentialDrive((MotorController)leftMotor, (MotorController)rightMotor);
+    diffControl.setExpiration(2);
+    diffControl.setSafetyEnabled(true);
+    
+    diffControl.curvatureDrive(0, 0, false);
+  }
+
+  public void rotate(double rotate) {
+    rightMotor.set(TalonSRXControlMode.PercentOutput, rotate * -.6);
+    leftMotor.set(TalonSRXControlMode.PercentOutput, rotate * .6);
+    
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public DifferentialDrive getDifferentialDrive() {
+    return diffControl;
+  }
+}
